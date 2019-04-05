bfe28a93f2fe56456e1dfed5ad57d8e845468457
Andy Burke <andy.burke@mailbox.earth>
2019-04-04 23:44:44 -0700

    update to ES6+, add tests

--- src/compiler.js
+++ src/compiler.js
@@ -2,129 +2,106 @@
 
 module.exports = compile;
 
-var generators = {
-    "NUMBER": function( node ) {
-        return node.arguments[ 0 ].indexOf( '.' ) !== -1 ? parseFloat( node.arguments[ 0 ] ) : parseInt( node.arguments[ 0 ], 10 );
-    },
-    "STRING": function( node ) {
-        return '"' + node.arguments[ 0 ].replace( '"', '\\"' ) + '"';
-    },
-    "BOOLEAN": function( node ) {
-        return node.arguments[ 0 ].toLowerCase() === 'true';
-    },
-    "PRIMITIVE": function( node ) {
-        var value = node.arguments[ 0 ];
-        switch ( value.toLowerCase() ) {
-            case 'null':
-                value = null;
-                break;
-            case 'undefined':
-                value = undefined;
-                break;
-        }
-        return value;
-    },
-    "SYMBOL": function( node ) {
-        return '' + node.arguments[ 0 ];
-    },
-    
-    "-": function( node ) {
-        return -(_processNode( node.arguments[ 0 ] ));
-    },
-    "&&": function( node ) {
-        var result = [];
-        node.arguments.forEach( function( _node ) {
-           result.push( _processNode( _node ) );
-        } );
-        return ' ' + result.join( ' && ' );
-    },
-    "||": function( node ) {
-        var result = [];
-        node.arguments.forEach( function( _node ) {
-            result.push( _processNode( _node ) );
-        } );
-        return ' ' + result.join( ' || ' ); 
-    },
-    "IN": function( node ) {
-        var value = _processNode( node.arguments[ 0 ] );
-        var field = _processNode( node.arguments[ 1 ] );
-        return ' ' + field + '.indexOf( ' + value + ' ) !== -1 ';
-    },
-    "!": function( node ) {
-        return ' !' + _processNode( node.arguments[ 0 ] );
-    },
-    "==": function( node ) {
-        var comparison = _extractComparison( node );
-        return ' ' + _processNode( comparison.symbol ) + ' == ' + _processNode( comparison.value );
-    },
-    "!=": function( node ) {
-        var comparison = _extractComparison( node );
-        return ' ' + _processNode( comparison.symbol ) + ' != ' + _processNode( comparison.value );
-    },
-    "MATCH": function( node ) {
-        var comparison = _extractComparison( node );
-        return ' ' + _processNode( comparison.symbol ) + '.match( new RegExp( ' + _processNode( comparison.value ) + ' ) )';
-    },
-    "<": function( node ) {
-        var comparison = _extractComparison( node );
-        return ' ' + _processNode( comparison.symbol ) + ' < ' + _processNode( comparison.value );
-    },    
-    "<=": function( node ) {
-        var comparison = _extractComparison( node );
-        return ' ' + _processNode( comparison.symbol ) + ' <= ' + _processNode( comparison.value );
-    },    
-    ">": function( node ) {
-        var comparison = _extractComparison( node );
-        return ' ' + _processNode( comparison.symbol ) + ' > ' + _processNode( comparison.value );
-    },    
-    ">=": function( node ) {
-        var comparison = _extractComparison( node );
-        return ' ' + _processNode( comparison.symbol ) + ' >= ' + _processNode( comparison.value );
-    },
-    "EXPRESSION": function( node ) {
-        var result = [];
-        node.arguments.forEach( function( _node ) {
-            result.push( _processNode( _node ) );
-        } );
-        return ' (' + result.join( ' ' ) + ') ';
-    }
-};
-
 function _extractComparison( node ) {
-    var symbol = null;
-    var value = null;
-    node.arguments.forEach( function( _node ) {
-        if ( _node.type === 'SYMBOL' ) {
-            if ( symbol ) {
-                throw new Error( 'JS: You can only specify one symbol in a comparison.' );
-            }
-            symbol = _node;
-        } else {
-            if ( value ) {
-                throw new Error( 'JS: You can only specify one value in a comparison.' );
-            }
-            value = _node;
-        }
-    } );
-
-    if ( !( symbol && value ) ) {
-        throw new Error( 'JS: Invalid comparison, could not find both symbol and value.' );
+    if ( node.arguments.length !== 2 ) {
+        throw new Error( 'JS: A comparison must have only to arguments, a left-hand-side and a right-hand-side.' );
     }
 
     return {
-        symbol: symbol,
-        value: value
+        lhs: node.arguments[ 0 ],
+        rhs: node.arguments[ 1 ]
     };
 }
+
+function _create_comparison_operator( operator ) {
+    return node => {
+        const comparison = _extractComparison( node );
+        return ` ${ _processNode( comparison.lhs ) } ${ operator } ${ _processNode( comparison.rhs ) } `;
+    };
+}
+
+const generators = {
+    'NUMBER': function( node ) {
+        return node.arguments[ 0 ].indexOf( '.' ) !== -1 ? parseFloat( node.arguments[ 0 ] ) : parseInt( node.arguments[ 0 ], 10 );
+    },
+    'STRING': function( node ) {
+        return `"${ node.arguments[ 0 ].replace( '"', '\\"' ) }"`;
+    },
+    'BOOLEAN': function( node ) {
+        return node.arguments[ 0 ].toLowerCase() === 'true' ? 'true' : 'false';
+    },
+    'PRIMITIVE': function( node ) {
+        switch ( node.arguments[ 0 ].toLowerCase() ) {
+            case 'null':
+                return 'null';
+            case 'undefined':
+                return 'undefined';
+            default:
+                throw new Error( `Unknown PRIMITIVE type: ${ node.arguments[ 0 ] }` );
+        }
+    },
+    'SYMBOL': function( node ) {
+        return node.arguments[ 0 ];
+    },
+    'ARRAY': function( node ) {
+        return `[ ${ node.arguments.map( arg => {
+            return _processNode( arg );
+        } ).join( ', ' ) } ]`;
+    },
+
+    '-': function( node ) {
+        return ` -( ${ _processNode( node.arguments[ 0 ] ) } ) `;
+    },
+    '&&': function( node ) {
+        const result = [];
+        node.arguments.forEach( function( _node ) {
+            result.push( _processNode( _node ) );
+        } );
+        return ` ${ result.join( ' && ' ) } `;
+    },
+    '||': function( node ) {
+        const result = [];
+        node.arguments.forEach( function( _node ) {
+            result.push( _processNode( _node ) );
+        } );
+        return ` ${ result.join( ' || ' ) } `;
+    },
+    'IN': function( node ) {
+        const value = _processNode( node.arguments[ 0 ] );
+        const field = _processNode( node.arguments[ 1 ] );
+        return ` ${ field }.includes( ${ value } ) `;
+    },
+    '!': function( node ) {
+        return ` !${ _processNode( node.arguments[ 0 ] ) } `;
+    },
+    '==': _create_comparison_operator( '==' ),
+    '!=': _create_comparison_operator( '!=' ),
+    '<': _create_comparison_operator( '<' ),
+    '<=': _create_comparison_operator( '<=' ),
+    '>': _create_comparison_operator( '>' ),
+    '>=': _create_comparison_operator( '>=' ),
+    'MATCH': function( node ) {
+        const comparison = _extractComparison( node );
+        return ` ${ _processNode( comparison.lhs ) }.match( new RegExp( ${ _processNode( comparison.rhs ) } ) ) `;
+    },
+    'EXPRESSION': function( node ) {
+        const result = [];
+        node.arguments.forEach( function( _node ) {
+            result.push( _processNode( _node ) );
+        } );
+        return ` ( ${ result.join( ' ' ) } ) `;
+    }
+};
 
 function _processNode( node ) {
     if ( !( node.type in generators ) ) {
         throw new Error( 'invalid node type' );
     }
-    
+
     return generators[ node.type ]( node );
 }
 
 function compile( tree ) {
-    return _processNode( tree );
+    const result = _processNode( tree );
+    return result.trim().replace( /\s+/g, ' ' );
 }
